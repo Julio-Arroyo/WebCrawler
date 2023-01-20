a1d9ceaf039522460ec273dd47cc6331400fa753
Julio-Arroyo <jarroyoi@caltech.edu>
2023-01-20 11:23:53 -0800

    Finish degree distribution calculation.

--- Crawler.cpp
+++ Crawler.cpp
@@ -36,12 +36,11 @@ std::vector<std::string> &fetch_links(const std::string &url, std::vector<std::s
     return urls;
 }
 
-void crawl_web(std::string start_url)
+void crawl_web(std::string start_url,
+               Graph &webgraph,
+               URLMap &urls,
+               std::unordered_map<std::string, Vertex> &url_to_vert)
 {
-    // Initialize data structures
-    Graph webgraph(0);
-    URLMap urls = get(vertex_name, webgraph);
-    std::unordered_map<std::string, Vertex> url_to_vert;
     std::vector<Vertex> stack;
 
     Vertex start_vertex = add_vertex(webgraph);
@@ -93,9 +92,64 @@ void crawl_web(std::string start_url)
     std::cout << std::endl;
 }
 
+std::pair<MapPtr, MapPtr> get_degree_dist(Graph &webgraph)
+{
+    MapPtr in_degree_dist(new std::unordered_map<int, int>());
+    MapPtr out_degree_dist(new std::unordered_map<int, int>());
+
+    VertIter vi, vi_end;
+    for (tie(vi, vi_end) = vertices(webgraph); vi != vi_end; vi++)
+    {
+        Vertex curr_vert = *vi;
+        Degree n_incoming = in_degree(curr_vert, webgraph);
+        Degree n_outgoing = out_degree(curr_vert, webgraph);
+
+        if (in_degree_dist->count(n_incoming) == 0)
+        {
+            (*in_degree_dist)[n_incoming] = 1;
+        } else
+        {
+            (*in_degree_dist)[n_incoming] += 1;
+        }
+
+        if (out_degree_dist->count(n_outgoing) == 0)
+        {
+            (*out_degree_dist)[n_outgoing] = 1;
+        } else
+        {
+            (*out_degree_dist)[n_outgoing] += 1;
+        }
+    }
+    std::pair<MapPtr, MapPtr> degree_dist(in_degree_dist, out_degree_dist);
+    return degree_dist;
+}
+
 int main()
 {
-    crawl_web("https://www.highlands.edu.sv/");
+    Graph webgraph(0);
+    URLMap urls = get(vertex_name, webgraph);  // PropertyMap to get URLs from vertex descriptors
+    std::unordered_map<std::string, Vertex> url_to_vert;
+
+    crawl_web("https://www.highlands.edu.sv/", webgraph, urls, url_to_vert);
+
+    MapPtr in_deg_dist, out_deg_dist;
+    tie(in_deg_dist, out_deg_dist) = get_degree_dist(webgraph);
+
+    // Record degree distributions
+    std::ofstream in_deg_f, out_deg_f;
+    in_deg_f.open("in_deg_dist.txt");
+    for (const std::pair<int, int> entry : (*in_deg_dist))
+    {
+        in_deg_f << entry.first << "," << entry.second << std::endl;
+    }
+    in_deg_f.close();
+
+    out_deg_f.open("out_deg_dist.txt");
+    for (const std::pair<int, int> entry : (*out_deg_dist))
+    {
+        out_deg_f << entry.first << "," << entry.second << std::endl;
+    }
+    out_deg_f.close();
     
     return 0;
 }
--- Crawler.hpp
+++ Crawler.hpp
@@ -3,8 +3,10 @@
 
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_traits.hpp>
+#include <boost/shared_ptr.hpp>
 #include <unordered_map>
 #include <iostream>
+#include <fstream>
 
 using namespace boost;
 
@@ -12,7 +14,7 @@ using namespace boost;
 typedef property<vertex_name_t, std::string> VertexProperty;
 typedef adjacency_list<setS, vecS, bidirectionalS, VertexProperty> Graph;  // template params: edge-list, vertex list, "directedness", vertex properties
 typedef property_map<Graph, vertex_name_t>::type URLMap;  // Map: vertex descriptor -> URL string
-
+typedef graph_traits<Graph>::degree_size_type Degree;
 // Vertex
 typedef graph_traits<Graph>::vertex_descriptor Vertex;
 
@@ -20,10 +22,17 @@ typedef graph_traits<Graph>::vertex_descriptor Vertex;
 typedef graph_traits<Graph>::vertex_iterator VertIter;
 typedef graph_traits<Graph>::edge_iterator EdgeIter;
 
+typedef shared_ptr<std::unordered_map<int, int> > MapPtr;
+
 const int MaxUrls = 20;
 const std::string Domain = "highlands.edu";
 
-void crawl_web(std::string start_url);
+void crawl_web(std::string start_url,
+               Graph &webgraph,
+               URLMap &urls,
+               std::unordered_map<std::string, Vertex> &url_to_vert);
+
 std::vector<std::string> &fetch_links(const std::string &url, std::vector<std::string> &urls);
+std::pair<MapPtr, MapPtr> get_degree_dist();
 
 #endif
